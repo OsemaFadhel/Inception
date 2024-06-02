6f66a7c746cca2e222ee84910b4b95cb2757e85a
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-02 18:34:50 +0200

    inc

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -3,24 +3,14 @@
 # Start MariaDB service
 service mariadb start
 
-# Create the database if it doesn't exist
-mysql -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+sleep 5
 
-# Create the user if it doesn't exist
-mysql -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${db_name}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${db_user}\`@'localhost' IDENTIFIED BY '${db_pwd}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO \`${db_user}\`@'%' IDENTIFIED BY '${db_pwd}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -p1234 -e "FLUSH PRIVILEGES;"
+mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# Grant privileges to the user
-mysql -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'localhost';"
-
-# Update the root user password
-mysql -u root -p"${DB_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-
-# Flush privileges
-mysql -u root -p"${DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-
-# Shutdown the MariaDB server
-mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
-
-# Start MariaDB safely
-exec mysqld_safe
+exec mariadbd-safe
 
