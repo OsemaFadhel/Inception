9aed055da5be8b0911ebd5b75627e1fe8e4ae584
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-02 19:00:42 +0200

    inc

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -5,12 +5,15 @@ service mariadb start
 
 sleep 5
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mariadb -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-mariadb-admin -u root -p$DB_ROOT_PASSWORD shutdown
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -u root -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%';"
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
 
+# Shutdown MariaDB
+mariadb-admin -u root -p${DB_ROOT_PASSWORD} shutdown
+
+# Start MariaDB in safe mode
 exec mariadbd-safe
 
