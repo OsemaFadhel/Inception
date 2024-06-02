728daf91848f198c95969a91b16f22618fc693f1
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-02 18:55:53 +0200

    inc

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -9,7 +9,7 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mariadb -p1234 -e "FLUSH PRIVILEGES;"
+mariadb -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 mariadb-admin -u root -p$DB_ROOT_PASSWORD shutdown
 
 exec mariadbd-safe
