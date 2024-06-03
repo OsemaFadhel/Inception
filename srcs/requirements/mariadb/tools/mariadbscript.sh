71acab519bff898bae5909f1d233820c23edbb65
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-03 09:00:41 +0200

    inc

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -4,11 +4,11 @@ service mariadb start
 
 sleep 5
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mariadb -u root -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 mariadb-admin -u root -p${DB_ROOT_PASSWORD} shutdown
 
 exec mariadbd-safe
