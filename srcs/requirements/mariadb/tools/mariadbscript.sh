f0371129033c886c286d38459057c4863b8866fb
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-03 18:03:32 +0200

    inception

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 # Start the MariaDB service
-systemctl start mariadb
-systemctl enable mariadb
+service mariadb start
 
 sleep 10
 
 # Create the database if it doesn't exist
 echo "Creating database if it doesn't exist..."
-mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb  -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
 # Create the user if it doesn't exist
 echo "Creating user if it doesn't exist..."
-mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb  -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 
 # Grant all privileges to the user on the database
 echo "Granting privileges..."
-mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;"
+mariadb  -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;"
 
 # Change the root user's password
 echo "Changing root user's password..."
-mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
 # Flush privileges to ensure changes take effect
 echo "Flushing privileges..."
-mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+mariadb  -e "FLUSH PRIVILEGES;"
 
 # Shut down the mariadb service
 echo "Shutting down MariaDB service..."
