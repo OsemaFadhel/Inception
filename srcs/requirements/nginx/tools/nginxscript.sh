a18e137c5090eb1764ef15d609ab3bebfdcd3de2
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-03 21:40:12 +0200

    nginx

--- srcs/requirements/nginx/tools/nginxscript.sh
+++ srcs/requirements/nginx/tools/nginxscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=${DOMAIN_NAME}/UID=${USER}"
+openssl req -x509 -nodes -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}/UID=${USER}"
 
-sed -i "s/domain_name/${DOMAIN_NAME}/g" "/etc/nginx/sites-available/nginx.conf"
+sed -i "s/domain_name/${DOMAIN_NAME}/g" "/etc/nginx/nginx.conf"
 
 nginx -g "daemon off;"
