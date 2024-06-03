6fb4a3799d003aaa1c91a5576dc4749228b34531
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-06-03 16:28:37 +0200

    inc

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Start the MariaDB service
-service mysql start
+systemctl start mariadb
+systemctl enable mariadb
 
 # Create the database if it doesn't exist
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -u -p{DB_ROOT_PASSWORD} "CREATE DATABASE \`${DB_NAME}\`;"
 
 # Create the user if it doesn't exist
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u -p{DB_ROOT_PASSWORD} "CREATE USER \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 
 # Grant all privileges to the user on the database
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u -p{DB_ROOT_PASSWORD} "GRANT ALL ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;"
 
 # Change the root user's password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mysql -u -p{DB_ROOT_PASSWORD} "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
 # Flush privileges to ensure changes take effect
-mysql -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mysql -u -p${DB_ROOT_PASSWORD} "FLUSH PRIVILEGES;"
 
 # Shut down the mysql service
 mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
